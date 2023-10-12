3e6f0262605db6400888e831b088e9202a35a429
Karrics <143880497+Karrics@users.noreply.github.com>
2023-10-12 17:41:56 +0300

    Add files via upload

--- Tymakov_6/Program.cs
+++ Tymakov_6/Program.cs
@@ -7,7 +7,12 @@ namespace Tymakov_6
     internal class Program
     {
 
-        // Метод для умножения двух матриц
+        /// <summary>
+        /// Метод для умножения двух матриц
+        /// </summary>
+        /// <param name="matrixA"></param>
+        /// <param name="matrixB"></param>
+        /// <returns></returns>
         static int[,] MultiplyMatrices(int[,] matrixA, int[,] matrixB)
         {
             int rowsA = matrixA.GetLength(0); // Количество строк в матрице A
@@ -31,7 +36,10 @@ namespace Tymakov_6
             return result;
         }
 
-        // Метод для печати матрицы
+        /// <summary>
+        /// Метод для печати матрицы
+        /// </summary>
+        /// <param name="matrix"></param>
         static void PrintMatrix(int[,] matrix)
         {
             int rows = matrix.GetLength(0); // Количество строк в матрице
@@ -50,6 +58,10 @@ namespace Tymakov_6
             Console.WriteLine();
         }
 
+        /// <summary>
+        /// Метод генерации рандомной температуры
+        /// </summary>
+        /// <returns></returns>
         static double[,] GenerateRandomTemperatures()
         {
             Random random = new Random();
@@ -66,7 +78,13 @@ namespace Tymakov_6
             return temperatures;
         }
 
-        static double[] CalculateAverageTemperatures(double[,] temperatures)
+
+        /// <summary>
+        /// Находит среднею температуру месяца 
+        /// </summary>
+        /// <param name="temperatures"></param>
+        /// <returns></returns>
+        static double[] AverageTemperatures(double[,] temperatures)
         {
             double[] averageTemperatures = new double[12];
 
@@ -95,6 +113,14 @@ namespace Tymakov_6
             return monthNames;
         }
 
+
+        /// <summary>
+        /// Создаёт матрицу с помощью List
+        /// </summary>
+        /// <param name="rows"></param>
+        /// <param name="cols"></param>
+        /// <param name="matrixName"></param>
+        /// <returns></returns>
         static LinkedList<LinkedList<int>> CreateMatrix(int rows, int cols, string matrixName)
         {
             LinkedList<LinkedList<int>> matrix = new LinkedList<LinkedList<int>>();
@@ -117,6 +143,13 @@ namespace Tymakov_6
 
             return matrix;
         }
+
+        /// <summary>
+        /// Перемножение матриц при помощи List
+        /// </summary>
+        /// <param name="matrixA"></param>
+        /// <param name="matrixB"></param>
+        /// <returns></returns>
         static LinkedList<LinkedList<int>> MultiplyMatrices(LinkedList<LinkedList<int>> matrixA, LinkedList<LinkedList<int>> matrixB)
         {
             int rowsA = matrixA.Count; // Количество строк в матрице A
@@ -155,7 +188,10 @@ namespace Tymakov_6
             return result;
         }
 
-        // Метод для печати матрицы
+        /// <summary>
+        /// Метод для печати матрицы
+        /// </summary>
+        /// <param name="matrix"></param>
         static void PrintMatrix(LinkedList<LinkedList<int>> matrix)
         {
             foreach (var row in matrix)
@@ -173,130 +209,139 @@ namespace Tymakov_6
     
         static void Main()
         {
-            Console.WriteLine("Упр 6.2");
-            Console.WriteLine("Умнажение двух матриц");
-            // Ввод размерности матрицы A
-            Console.Write("Введите количество строк матрицы A: ");
-            int rowsA = int.Parse(Console.ReadLine());
-
-            Console.Write("Введите количество столбцов матрицы A: ");
-            int colsA = int.Parse(Console.ReadLine());
-
-            // Ввод размерности матрицы B
-            Console.Write("Введите количество строк матрицы B: ");
-            int rowsB = int.Parse(Console.ReadLine());
-
-            Console.Write("Введите количество столбцов матрицы B: ");
-            int colsB = int.Parse(Console.ReadLine());
-
-            // Проверка возможности умножения матриц
-            if (colsA != rowsB)
+            try
             {
-                Console.WriteLine("Умножение матриц невозможно. Количество столбцов матрицы A должно быть равно количеству строк матрицы B.");
-                return;
-            }
+                Console.WriteLine("Упр 6.2");
+                Console.WriteLine("Умнажение двух матриц");
+                // Ввод размерности матрицы A
+                Console.Write("Введите количество строк матрицы A: ");
+                int rowsA = int.Parse(Console.ReadLine());
 
-            // Создание и инициализация матрицы A
-            int[,] matrixA = new int[rowsA, colsA];
-            Console.WriteLine("Введите элементы матрицы A:");
+                Console.Write("Введите количество столбцов матрицы A: ");
+                int colsA = int.Parse(Console.ReadLine());
 
-            for (int i = 0; i < rowsA; i++)
-            {
-                for (int j = 0; j < colsA; j++)
+                // Ввод размерности матрицы B
+                Console.Write("Введите количество строк матрицы B: ");
+                int rowsB = int.Parse(Console.ReadLine());
+
+                Console.Write("Введите количество столбцов матрицы B: ");
+                int colsB = int.Parse(Console.ReadLine());
+
+                // Проверка возможности умножения матриц
+                if (colsA != rowsB)
                 {
-                    Console.Write($"Элемент [{i+1}, {j+1}]: ");
-                    matrixA[i, j] = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Умножение матриц невозможно. Количество столбцов матрицы A должно быть равно количеству строк матрицы B.");
+                    return;
                 }
-            }
 
-            // Создание и инициализация матрицы B
-            int[,] matrixB = new int[rowsB, colsB];
-            Console.WriteLine("Введите элементы матрицы B:");
+                // Создание и инициализация матрицы A
+                int[,] matrixA = new int[rowsA, colsA];
+                Console.WriteLine("Введите элементы матрицы A:");
 
-            for (int i = 0; i < rowsB; i++)
-            {
-                for (int j = 0; j < colsB; j++)
+                for (int i = 0; i < rowsA; i++)
                 {
-                    Console.Write($"Элемент [{i+1}, {j+1}]: ");
-                    matrixB[i, j] = int.Parse(Console.ReadLine());
+                    for (int j = 0; j < colsA; j++)
+                    {
+                        Console.Write($"Элемент [{i + 1}, {j + 1}]: ");
+                        matrixA[i, j] = int.Parse(Console.ReadLine());
+                    }
                 }
+
+                // Создание и инициализация матрицы B
+                int[,] matrixB = new int[rowsB, colsB];
+                Console.WriteLine("Введите элементы матрицы B:");
+
+                for (int i = 0; i < rowsB; i++)
+                {
+                    for (int j = 0; j < colsB; j++)
+                    {
+                        Console.Write($"Элемент [{i + 1}, {j + 1}]: ");
+                        matrixB[i, j] = int.Parse(Console.ReadLine());
+                    }
+                }
+
+                // Вычисление результирующей матрицы C
+                int[,] matrixC = MultiplyMatrices(matrixA, matrixB);
+
+                // Вывод матрицы A
+                Console.WriteLine("Матрица A:");
+                PrintMatrix(matrixA);
+
+                // Вывод матрицы B
+                Console.WriteLine("Матрица B:");
+                PrintMatrix(matrixB);
+
+                // Вывод результирующей матрицы C
+                Console.WriteLine("Результирующая матрица C:");
+                PrintMatrix(matrixC);
+                Console.WriteLine();
+
+
+                Console.WriteLine("Упр 6.3");
+                Console.WriteLine("Средняя температура за год");
+                double[,] temperatures = GenerateRandomTemperatures();
+                double[] averageTemperatures = AverageTemperatures(temperatures);
+                string[] monthNames = GetMonthNames();
+
+                Array.Sort(averageTemperatures, monthNames); // Сортировка массивов averageTemperatures и monthNames вместе
+
+                Console.WriteLine("Средние температуры по месяцам (в градусах Цельсия):");
+                for (int i = 0; i < averageTemperatures.Length; i++)
+                {
+                    Console.WriteLine($"{monthNames[i]}: {averageTemperatures[i]}");
+                }
+                Console.WriteLine();
+
+                Console.WriteLine("Дз 6.2");
+                Console.WriteLine("Умножение матриц через List");
+                Console.Write("Введите количество строк матрицы A: ");
+                int rowsA_list = int.Parse(Console.ReadLine());
+
+                Console.Write("Введите количество столбцов матрицы A: ");
+                int colsA_list = int.Parse(Console.ReadLine());
+
+                // Ввод размерности матрицы B
+                Console.Write("Введите количество строк матрицы B: ");
+                int rowsB_list = int.Parse(Console.ReadLine());
+
+                Console.Write("Введите количество столбцов матрицы B: ");
+                int colsB_list = int.Parse(Console.ReadLine());
+
+                // Проверка возможности умножения матриц
+                if (colsA != rowsB)
+                {
+                    Console.WriteLine("Умножение матриц невозможно. Количество столбцов матрицы A должно быть равно количеству строк матрицы B.");
+                    return;
+                }
+
+                // Создание и инициализация матрицы A
+                LinkedList<LinkedList<int>> matrixA_list = CreateMatrix(rowsA, colsA, "A");
+
+                // Создание и инициализация матрицы B
+                LinkedList<LinkedList<int>> matrixB_list = CreateMatrix(rowsB, colsB, "B");
+
+                // Вычисление результирующей матрицы C
+                LinkedList<LinkedList<int>> matrixC_list = MultiplyMatrices(matrixA_list, matrixB_list);
+
+                // Вывод матрицы A
+                Console.WriteLine("Матрица A:");
+                PrintMatrix(matrixA);
+
+                // Вывод матрицы B
+                Console.WriteLine("Матрица B:");
+                PrintMatrix(matrixB);
+
+                // Вывод результирующей матрицы C
+                Console.WriteLine("Результирующая матрица C:");
+                PrintMatrix(matrixC);
+                Console.ReadKey();
             }
-
-            // Вычисление результирующей матрицы C
-            int[,] matrixC = MultiplyMatrices(matrixA, matrixB);
-
-            // Вывод матрицы A
-            Console.WriteLine("Матрица A:");
-            PrintMatrix(matrixA);
-
-            // Вывод матрицы B
-            Console.WriteLine("Матрица B:");
-            PrintMatrix(matrixB);
-
-            // Вывод результирующей матрицы C
-            Console.WriteLine("Результирующая матрица C:");
-            PrintMatrix(matrixC);
-            Console.WriteLine();
-
-
-            Console.WriteLine("Упр 6.3");
-            Console.WriteLine("Средняя температура за год");
-            double[,] temperatures = GenerateRandomTemperatures();
-            double[] averageTemperatures = CalculateAverageTemperatures(temperatures);
-            string[] monthNames = GetMonthNames();
-
-            Array.Sort(averageTemperatures, monthNames); // Сортировка массивов averageTemperatures и monthNames вместе
-
-            Console.WriteLine("Средние температуры по месяцам (в градусах Цельсия):");
-            for (int i = 0; i < averageTemperatures.Length; i++)
+            catch (FormatException)
             {
-                Console.WriteLine($"{monthNames[i]}: {averageTemperatures[i]}");
+                Console.WriteLine("Неккоректно введено число");
+                Console.ReadKey();
             }
             Console.WriteLine();
-
-            Console.WriteLine("Дз 6.2");
-            Console.WriteLine("Умножение матриц через List");
-            Console.Write("Введите количество строк матрицы A: ");
-            int rowsA_list = int.Parse(Console.ReadLine());
-
-            Console.Write("Введите количество столбцов матрицы A: ");
-            int colsA_list = int.Parse(Console.ReadLine());
-
-            // Ввод размерности матрицы B
-            Console.Write("Введите количество строк матрицы B: ");
-            int rowsB_list = int.Parse(Console.ReadLine());
-
-            Console.Write("Введите количество столбцов матрицы B: ");
-            int colsB_list = int.Parse(Console.ReadLine());
-
-            // Проверка возможности умножения матриц
-            if (colsA != rowsB)
-            {
-                Console.WriteLine("Умножение матриц невозможно. Количество столбцов матрицы A должно быть равно количеству строк матрицы B.");
-                return;
-            }
-
-            // Создание и инициализация матрицы A
-            LinkedList<LinkedList<int>> matrixA_list = CreateMatrix(rowsA, colsA, "A");
-
-            // Создание и инициализация матрицы B
-            LinkedList<LinkedList<int>> matrixB_list = CreateMatrix(rowsB, colsB, "B");
-
-            // Вычисление результирующей матрицы C
-            LinkedList<LinkedList<int>> matrixC_list = MultiplyMatrices(matrixA_list, matrixB_list);
-
-            // Вывод матрицы A
-            Console.WriteLine("Матрица A:");
-            PrintMatrix(matrixA);
-
-            // Вывод матрицы B
-            Console.WriteLine("Матрица B:");
-            PrintMatrix(matrixB);
-
-            // Вывод результирующей матрицы C
-            Console.WriteLine("Результирующая матрица C:");
-            PrintMatrix(matrixC);
-            Console.ReadKey();
         }
     }
 }
